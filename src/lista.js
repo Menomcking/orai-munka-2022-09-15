24eb9e7cb642de703934e895de2a2b9aaf99db14
Menomcking <patrik8870@gmail.com>
2022-09-15 12:32:19 +0200

    2. feladat kész

---
+++ src/lista.js
@@ -0,0 +1,19 @@
+const list = [];
+export function hozaad(){
+    for(let i = 0; i = 10; i++){
+        if(szam < 0){
+        print("Hibás számot adott meg.")
+        }else{
+        list[i] = szam;
+        }
+    }
+}
+export function atlag(){
+    let szamolo = 0;
+    let osszegszam = 0;
+    for(let i = 0; i = 10; i++){
+        osszegszam= osszegszam + szam;
+        szamolo++;
+    }
+    let atlagszam = osszegszam / szamolo
+}
